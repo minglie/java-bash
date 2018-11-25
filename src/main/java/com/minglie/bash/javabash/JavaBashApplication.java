0ac1939fad5ecaf9ef534e5fca47a8dbfaf8d806
minglie <934031452@qq.com>
2018-11-26 01:25:38 +0800

    aaa

---
+++ src/main/java/com/minglie/bash/javabash/JavaBashApplication.java
@@ -0,0 +1,12 @@
+package com.minglie.bash.javabash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBashApplication.class, args);
+	}
+}
